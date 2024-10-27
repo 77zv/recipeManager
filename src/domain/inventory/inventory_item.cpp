99d5cb8d5b27e69d86499ed7a953985c8e841ca8
tonyy.lb <antonylbrussolo@gmail.com>
2024-10-27 14:01:16 -0400

    implementations for the inventory domain

---
+++ src/domain/inventory/inventory_item.cpp
@@ -0,0 +1,23 @@
+#include "inventory_item.hpp"
+
+InventoryItem::InventoryItem(Ingredient ingredient, Quantity quantity) : ingredient(ingredient), quantity(quantity) {}
+
+Ingredient InventoryItem::getIngredient() const {
+    return ingredient;
+}
+
+Quantity InventoryItem::getQuantity() const {
+    return quantity;
+}
+
+void InventoryItem::setQuantity(Quantity quantity) {
+    this->quantity = quantity;
+}
+
+void InventoryItem::addQuantity(Quantity quantity) {
+    this->quantity = this->quantity + quantity;
+}
+
+void InventoryItem::removeQuantity(Quantity quantity) { 
+    this->quantity = this->quantity - quantity;
+}
