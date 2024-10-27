c3e4668780d7b79ccaa9a9dc4a818bc93f1c063f
tonyy.lb <antonylbrussolo@gmail.com>
2024-10-27 14:01:00 -0400

    declarations for the inventory domain

---
+++ src/domain/inventory/inventory_item.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "../ingredient/ingredient.hpp"
+
+class InventoryItem {
+private:
+    Ingredient ingredient;
+    Quantity quantity;
+
+public:
+    InventoryItem(Ingredient ingredient, Quantity quantity);
+    Ingredient getIngredient() const;
+    Quantity getQuantity() const;
+    void setQuantity(Quantity quantity);
+    void addQuantity(Quantity quantity);
+    void removeQuantity(Quantity quantity);
+};
