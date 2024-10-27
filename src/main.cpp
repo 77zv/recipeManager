81b6b9924719a736c079cd5cc965595031195a3a
tonyy.lb <antonylbrussolo@gmail.com>
2024-10-27 13:22:09 -0400

    test ingredient domain classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "domain/ingredient/ingredient.hpp"
+#include "domain/ingredient/quantity.hpp"
+#include "domain/ingredient/unit.hpp"
 
 int main(int, char**){
+
+    Ingredient ingredient("Flour", Quantity(1, Unit::KILOGRAMS));
+
+    std::cout << ingredient.getName() << " " << ingredient.getQuantity().getAmount() << " " << unitToString(ingredient.getQuantity().getUnit()) << "\n";
+
     std::cout << "Hello, from recipeManager!\n";
 }
         
