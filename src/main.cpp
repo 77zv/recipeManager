f2074a0be2ef26785762c1324a14e54c46b1dde3
tonyy.lb <antonylbrussolo@gmail.com>
2024-10-27 13:52:40 -0400

    testing new tech (conversion and +/- operators)

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@
 
 int main(int, char**){
 
-    Ingredient ingredient("Flour", Quantity(1, Unit::KILOGRAMS));
+    Ingredient ingredient("Flour", Quantity(1, Unit::LITERS));
+    Ingredient ingredient2("Sugar", Quantity(3, Unit::MILLILITERS));
+
+    Quantity sum = ingredient.getQuantity() + ingredient2.getQuantity();
+    
+    std::cout << sum.getAmount() << " " << unitToString(sum.getUnit()) << "\n";
 
     std::cout << ingredient.getName() << " " << ingredient.getQuantity().getAmount() << " " << unitToString(ingredient.getQuantity().getUnit()) << "\n";
 
